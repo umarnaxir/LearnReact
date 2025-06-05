a5bf3f23d48df8060d01ea48782b8969c1f44da8
umarnaxir <erumarnazir@gmail.com>
2025-06-05 21:06:21 +0530

    Updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Post from "@/Components/Hooks/Custom Hook/Fetch";
 import Counter from "@/Components/Re-rendering/Counter";
 import Basic from "@/Components/Re-rendering/Basic"
+import App from "@/Components/Basic Program/Count"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       {/* <Post/> */}
       <Counter/>
       {/* <Basic/> */}
+      <Count/>
     </div>
   );
 }
