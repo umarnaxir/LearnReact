62bc645dd37bd1b36061433039c63c11eaf1208a
umarnaxir <erumarnazir@gmail.com>
2025-06-10 20:49:56 +0530

    Updated

--- src/Components/basic.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export function HelloWorld() {
-  return <h2>Hello World</h2>;
-}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import basic, { HelloWorld } from '../Components/basic.jsx';
+import CharacterCounter from '@/Components/Hooks/useEffect/CharacterCounter';
 function App() {
   return (
     <div>
-      <h1>Welcome to My App</h1>
-      <p>This is a simple React application.</p>
-      <style>
-        {`
-          h1 {
-            color: blue;
-          }
-          p {
-            font-size: 18px;
-            color: green;
-          }
-            
-        `}
-      </style>
-      <HelloWorld />
+      <CharacterCounter />
     </div>
   );
 }
