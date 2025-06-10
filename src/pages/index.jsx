0ac03d08b2a52e5523610916712631982da49982
umarnaxir <erumarnazir@gmail.com>
2025-06-10 21:06:14 +0530

    Updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CharacterCounter from '@/Components/Hooks/useEffect/CharacterCounter';
+import UpdateData from '@/Components/Hooks/useState/UpdateData';
 function App() {
   return (
     <div>
       <CharacterCounter />
+      <UpdateData />
     </div>
   );
 }
