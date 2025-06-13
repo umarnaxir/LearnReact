82404de596633b072b0c059925d78e646f4a36f4
umarnaxir <erumarnazir@gmail.com>
2025-06-13 15:28:20 +0530

    Updated

--- src/Components/Props/User.jsx
+++ src/Components/Props/User.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
 
-function User({name, phone, email}) {
+function User({User}) {
+
   return (
     <div>
-       <h2>User Component</h2> 
-       <hr></hr>
-       <h3>Name: {name}</h3>
-       <h3>Phone No: {phone}</h3>
-       <h3>Email ID: {email}</h3>
+       <h3>Name: {User.name}</h3>
+       <h3>Phone No: {User.phone}</h3>
+       <h3>Email ID: {User.email}</h3>
        <hr></hr>
     </div>
   )
 }
 
 export default User
+
+
+// import React from 'react';
+// import User from '@/Components/Props/User';
+// function App() {
+//   let object1 = {
+//     name: "Umar Nazir",
+//     phone: 7051732616,
+//     email: "umar@gmail.com"
+//   };
+//   let object2 = {
+//     name: "Syed Owais",
+//     phone: 1234567890,
+//     email: "owais@gmail.com"
+//   };
+//   let object3 = {
+//     name: "Khalid Jan",
+//     phone: 987654321,
+//     email: "khalid@gmail.com"
+//   };
+//   return (
+//     <div>
+//       <h1>Props in React Js</h1>
+//       <hr></hr>
+//       <User User={object1} />
+//       <User User={object2} />
+//       <User User={object3} />
+//       {/* <User name = "Umar" phone={7051732616} email={"umar@gmail.com"}/> */}
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import User from '@/Components/Props/User';
 function App() {
+  let object1 = {
+    name: "Umar Nazir",
+    phone: 7051732616,
+    email: "umar@gmail.com"
+  };
+  let object2 = {
+    name: "Syed Owais",
+    phone: 1234567890,
+    email: "owais@gmail.com"
+  };
+  let object3 = {
+    name: "Khalid Jan",
+    phone: 987654321,
+    email: "khalid@gmail.com"
+  };
   return (
     <div>
       <h1>Props in React Js</h1>
       <hr></hr>
-      <User name = "Umar" phone={7051732616} email={"umar@gmail.com"}/>
+      <User User={object1} />
+      <User User={object2} />
+      <User User={object3} />
+      {/* <User name = "Umar" phone={7051732616} email={"umar@gmail.com"}/> */}
     </div>
   );
 }
