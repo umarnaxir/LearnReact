438d92fcb3a7a3eba3bd04fff3b3c56f3cef8d2c
umarnaxir <erumarnazir@gmail.com>
2025-06-16 00:11:18 +0530

    Added

--- src/Components/Event Handlers/BasicEvent.jsx
+++ src/Components/Event Handlers/BasicEvent.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 function BasicEvent() {
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState("Umar");
     const handleClick = () =>{
         alert(`You clicked`);
+    }
+    const handleChange = (e) => {
+        setName(e.target.value);
     }
   return (
     <div>
         <button onClick={handleClick}>Click me</button>
         <br />
         <br />
-        <input type="text" />
+        <input type="text" value={name} onChange={handleChange} />
+        <button onClick={() => alert(`Submitted name: ${name}`)}>Submit</button>
     </div>
   )
 }
